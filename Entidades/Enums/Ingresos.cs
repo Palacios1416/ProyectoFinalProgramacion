e1889d1a16c5472619d51bbaf64c49ff56e6683a
Palacios1416 <130609254+Palacios1416@users.noreply.github.com>
2023-11-16 07:26:04 -0600

    Update by Dereck about the Ingresos calculo

--- Entidades/Enums/Ingresos.cs
+++ Entidades/Enums/Ingresos.cs
@@ -8,14 +8,59 @@ namespace Entidades.Enums
 {
     public enum Ingresos
     {
-        Antiguedad,
-        RiesgoLaboral,
-        Nocturnidad,
-        HorasExtras,
-        Viatico,
-        Comision,
-        DiasExtras,
-        Indemnizacion,
-        Depreciacion
+    // Factores para el cálculo de ingresos
+    private double factorAntiguedad = 0.05;
+    private double factorRiesgoLaboral = 0.1;
+    private double factorNocturnidad = 0.2;
+    private double factorHorasExtras = 15.0;
+    private double factorViatico = 30.0;
+    private double factorComision = 0.02;
+    private double factorDiasExtras = 20.0;
+    private double factorIndemnizacion = 200.0;
+    private double factorDepreciacion = 100.0;
+
+    // Método para calcular los ingresos
+    public double CalcularIngresos(double salarioBase, int antiguedad, bool riesgoLaboral,
+                                   int horasExtras, bool nocturnidad, double viatico,
+                                   double comision, int diasExtras, double indemnizacion,
+                                   double depreciacion)
+    {
+        double ingresos = salarioBase;
+
+        // Calcular ingresos por antigüedad
+        ingresos += salarioBase * antiguedad * factorAntiguedad;
+
+        // Calcular ingresos por riesgo laboral
+        if (riesgoLaboral)
+        {
+            ingresos += salarioBase * factorRiesgoLaboral;
+        }
+
+        // Calcular ingresos por nocturnidad
+        if (nocturnidad)
+        {
+            ingresos += salarioBase * factorNocturnidad;
+        }
+
+        // Calcular ingresos por horas extras
+        ingresos += horasExtras * factorHorasExtras;
+
+        // Calcular ingresos por viático
+        ingresos += viatico * factorViatico;
+
+        // Calcular ingresos por comisión
+        ingresos += comision * factorComision;
+
+        // Calcular ingresos por días extras
+        ingresos += diasExtras * factorDiasExtras;
+
+        // Calcular ingresos por indemnización
+        ingresos += indemnizacion * factorIndemnizacion;
+
+        // Calcular ingresos por depreciación
+        ingresos += depreciacion * factorDepreciacion;
+
+        return ingresos;
+    }
     }
 }
